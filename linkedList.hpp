aa44b073c67c52e799a29f10234cf677d50bfb6d
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2016-06-30 22:55:27 +0300

    Update linkedList.hpp

--- linkedList.hpp
+++ linkedList.hpp
@@ -2,10 +2,11 @@
 #define __linked_list_hpp
 
 
-#include <stdint.h>
+#include <cstddef>
 #include <exception>
 #include <vector>
 
+#define SIZE_MAX (size_t)-1
 
 /**
  * <h1>LinkedList</h1>
@@ -17,14 +18,14 @@ private:
 	 */
 	template <class T2> struct Node {
 		struct Node* pPrev;
-		struct Node* pNext;	
+		struct Node* pNext;
 		T2 item;
 	};
-	
-	Node<T>* pHead;	
-	Node<T>* pTail;	
+
+	Node<T>* pHead;
+	Node<T>* pTail;
 	mutable Node<T>* pIndex;
-	uint32_t itemCount;	
+	size_t itemCount;
 
 	/**
 	 * Removes the item pointed to by list index.
@@ -42,9 +43,9 @@ private:
 	/**
 	 * Custom exception object class.
 	 */
-	class LinkedListExcept : public std::exception {	
+	class LinkedListExcept : public std::exception {
 	public:
-		typedef const enum 
+		typedef const enum
 		{
 			ERR_EMPTY_LIST=0,
 			ERR_NO_PREV=1,
@@ -53,14 +54,14 @@ private:
 			INDEX_OUT_OF_RANGE=4
 		} errCodes;
 		errCodes m_error;
-		
+
 		LinkedListExcept(const errCodes error) : m_error(error) {}
-	
-		const char* what() const noexcept 
+
+		const char* what() const noexcept
 		{
 			return errStrings[m_error];
 		}
-		
+
 		static const char* errStrings[];
 	};
 
@@ -69,7 +70,7 @@ public:
 	 * Ctor.
 	 */
 	LinkedList();
-	
+
 	/**
 	 * Dtor.
 	 */
@@ -80,49 +81,49 @@ public:
 	 * @param `obj` the LinkedList object to copy.
 	 */
 	LinkedList(const LinkedList<T>& obj);
-	
+
 	/**
 	 * Add new item at the tail of the list and move the list index to the tail of the list.
 	 * @param `item` the item to be pushed at the tail of the list.
-	 * @throws ERR_LIST_FULL if node count equals UINT32_MAX.
+	 * @throws ERR_LIST_FULL if node count equals SIZE_MAX.
 	 */
 	void push(T item);
-	
+
 	/**
 	 * Remove item from the tail of the list and move the list index to the tail of the list.
 	 * @return the item at the tail of the list.
 	 * @throws ERR_EMPTY_LIST if list is empty.
 	 */
 	T pop();
-	
+
 	/**
 	 * Add new item at the head of the list and move the list index to the head of the list.
 	 * @param `item` the item to be inserted at the head of the list.
-	 * @throws ERR_LIST_FULL if node count equals UINT32_MAX.
+	 * @throws ERR_LIST_FULL if node count equals SIZE_MAX.
 	 */
 	void enqueue(T item);
-	
+
 	/**
 	 * Remove item from the head of the list and move the list index to the head of the list.
 	 * @return the item at the head of the list.
 	 * @throws ERR_EMPTY_LIST if list is empty.
 	 */
 	T dequeue();
-	
+
 	/**
 	 * Insert new item at provided index and move the list index to the new node.
 	 * @param `item` the item to be inserted to the list.
 	 * @param `index` the index at which to insert the item.
 	 * @return true if insert was successful.
 	 */
-	bool insert(T item, uint32_t index);
-	
+	bool insert(T item, size_t index);
+
 	/**
 	 * Remove item at provided index and move the list index to the provided index.
 	 * @param `index` the index of the item to remove.
 	 * @return true if remove was successful.
 	 */
-	bool removeAt(uint32_t index);
+	bool removeAt(size_t index);
 
 	/**
 	 * Remove the first instance of item and move the index to the previouse node.
@@ -136,15 +137,15 @@ public:
 	 * @param `item` the item to be removed.
 	 * @return the number of instances removed.
 	 */
-	uint32_t removeAll(T item);
-	
+	size_t removeAll(T item);
+
 	/**
 	 * Replace item at provided index and move the list index to the provided index.
 	 * @param `item` the new item.
 	 * @param `index` the index of the item to replace.
 	 * @return true if replace was successful.
 	 */
-	bool replace(T item, uint32_t index);
+	bool replace(T item, size_t index);
 
 	/**
 	 * Returns the last value in the list.
@@ -162,7 +163,7 @@ public:
 	 * @return the item at provided index.
 	 * @throws INDEX_OUT_OF_RANGE if index greater than item count.
 	 */
-	T at(uint32_t index) const;
+	T at(size_t index) const;
 
 	/**
 	 * Search the list for the provided item.
@@ -180,66 +181,66 @@ public:
 	 * Returns a vector containing all the items in the list or NULL if list is empty.
 	 */
 	std::vector<T>* toVector() const;
-	
+
 	/**
 	 * Get the current list index.
 	 */
-	uint32_t getCurrIndex() const;
+	size_t getCurrIndex() const;
 
 	/**
 	 * Return the index of the next instance of item.
 	 * @param `item` the item to search for.
-	 * @return the index of the next instance of item or UINT32_MAX if not found.
+	 * @return the index of the next instance of item or SIZE_MAX if not found.
 	 */
-	uint32_t nextIndexOf(T item) const;
+	size_t nextIndexOf(T item) const;
 
 	 /**
 	  * Return the index of the previouse instance of item.
 	  * @param `item` the item to search for.
-	  * @return the index of the previouse instance of item or UINT32_MAX if not found.
+	  * @return the index of the previouse instance of item or SIZE_MAX if not found.
 	  */
-	uint32_t prevIndexOf(T item) const;
+	size_t prevIndexOf(T item) const;
 
 	/**
 	 * Get the index of the first instans of item.
 	 * @param `item` the item to search for.
-	 * @return the index of the first item instance or UINT32_MAX if not found.
+	 * @return the index of the first item instance or SIZE_MAX if not found.
 	 */
-	uint32_t indexOf(T item) const;
+	size_t indexOf(T item) const;
 
 	/**
 	 * Get the index of the last instans of item.
 	 * @param `item` the item to search for.
-	 * @return the index of the last item instance or UINT32_MAX if not found.
+	 * @return the index of the last item instance or SIZE_MAX if not found.
 	 */
-	uint32_t lastIndexOf(T item) const;
+	size_t lastIndexOf(T item) const;
 
 	/**
-	 * Return a vector containing indexs of all item instances. 
+	 * Return a vector containing indexs of all item instances.
 	 * @param `item` the item to search for.
-	 * @return a vector of uint32_t values or NULL if not found.
+	 * @return a vector of size_t values or NULL if not found.
 	 */
-	std::vector<uint32_t>* allIndexsOf(T item) const;
-	
+	std::vector<size_t>* allIndexsOf(T item) const;
+
 	/**
 	 * Go to next item.
 	 * @return the next item.
 	 * @throws ERR_NO_NEXT if index at tail of list.
 	 */
 	T next() const;
-	
+
 	/**
 	 * Go to previous item.
 	 * @return the previous item.
 	 * @throws ERR_NO_PREV if index at head of list.
 	 */
 	T prev() const;
-	
+
 	/**
 	 * Returns true if list has next item or false otherwise.
 	 */
 	bool hasNext() const;
-	
+
 	/**
 	 * Returns true if list has previous item or false otherwise.
 	 */
@@ -248,7 +249,7 @@ public:
 	/**
 	 * Getter for the `itemCount` member variable.
 	 */
-	uint32_t count() const;
+	size_t count() const;
 };
 
 
